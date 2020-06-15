ac9c4320c217b3bc9120e01c85f032c22667a405
christiano39 <carneson39@gmail.com>
2020-06-15 13:52:11 -0600

    added MovieCard component

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link, useRouteMatch } from 'react-router-dom'
 
+import MovieCard from './MovieCard'
+
 const MovieList = props => {
   return (
     <div className="movie-list">
@@ -12,26 +14,11 @@ const MovieList = props => {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  //const { title, director, metascore, stars } = movie;
   const { url } = useRouteMatch() 
   return (
     <Link to={`${url}movies/${movie.id}`}>
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard movie={movie} />
     </Link>
   );
 }
