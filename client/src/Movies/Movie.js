6633b8e57c4cad4ae540c28e144b897ec408e4f4
christiano39 <carneson39@gmail.com>
2020-06-15 13:44:18 -0600

    minor refactor

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -7,7 +7,7 @@ const Movie = (props) => {
   const { id } = useParams()
  
   useEffect(() => {
-    // const id = 1
+    //const { id } = useParams()
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -20,7 +20,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[ id ]);
+  },[id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
